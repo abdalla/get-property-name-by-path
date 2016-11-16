6f4cbdd5ceef84f88b06f7069074b07dedc7bf47
Carlos Abdalla <abdalla@msdev.com.br>
2016-11-16 15:16:53 -0500

    example of reduce

---
+++ index.js
@@ -0,0 +1,22 @@
+let obj = {
+    name: "Joe",
+    car: {
+        brand: {
+            name: "F150",
+            year: 2016
+
+        }
+    }
+};
+
+let getPropertValueByPath = (obj, path) => {
+    let paths = path.split('.');
+    
+    return paths.reduce((prev, curr) => {
+        return prev[curr];
+    }, obj);
+};
+
+let pValue = getPropertValueByPath(obj, 'car.brand.name');
+
+console.log(pValue);
